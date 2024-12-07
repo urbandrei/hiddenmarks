b84077c81c8c2aecca824abaf9af0d0a8012b345
Andrei Roman <43176471+urbandrei@users.noreply.github.com>
2024-12-07 01:47:06 +0000

    Started working on player views

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
 
 class Player:
-    def __init__(self, mark):
-        self.hand = []
+    def __init__(self, mark, hand, alive):
+        self.hand = hand
         self.bank = []
+        self.knowledge = [[0]*5]*5
         self.mark = mark
+        self.alive = alive
 
 class Game:
     def __init__(self, numPlayers):
@@ -15,17 +17,69 @@ class Game:
         random.shuffle(self.unusedMarks)
         self.extraMark = self.unusedMarks.pop()
         self.players = []
-        for i in range(numPlayers):
-            self.players.append(Player(self.unusedMarks.pop()))
-            for j in range(3):
-                self.players[i].hand.append(self.drawPile.pop())
+        for i in range(4):
+            if i<numPlayers:
+                hand = []
+                for j in range(3):
+                    hand.append(self.drawPile.pop())
+                self.players.append(Player(self.unusedMarks.pop(),hand, True))
+            else:
+                self.players.append(Player(self.unusedMarks.pop(),[], False))
 
         self.currentPlayer = 0
         self.action = 0
 
-    def view(self, player):
-        print(self.players[player].hand)
+    def view(self):
+        v = []
+        for i in self.drawPile:
+            if i<22:
+                v.extend([1,0,0])
+            elif i<38:
+                v.extend([0,1,0])
+            else:
+                v.extend([0,0,1])
+        for i in range(48-len(self.drawPile)):
+            v.extend([0,0,0])
+        for i in range(4):
+            hr = 0
+            hb = 0
+            hw = 0
+            br = 0
+            bb = 0
+            bw = 0
+            for j in self.players[i].hand:
+                if j<22:
+                    hw += 1
+                elif j<38:
+                    hb += 1
+                else:
+                    hr += 1
+            for j in self.players[i].bank:
+                if j<22:
+                    bw += 1
+                elif j<38:
+                    bb += 1
+                else:
+                    br += 1
+            v.extend([1]*hw)
+            v.extend([0]*(22-hw))
+            v.extend([1]*hb)
+            v.extend([0]*(22-hb))
+            v.extend([1]*hr)
+            v.extend([0]*(22-hr))
+            v.extend([1]*bw)
+            v.extend([0]*(22-bw))
+            v.extend([1]*bb)
+            v.extend([0]*(22-bb))
+            v.extend([1]*br)
+            v.extend([0]*(22-br))
 
-newGame = Game(2)
+        return v
 
-newGame.view(1)
+    def possible(self):
+        return []
+
+    def action(self, out):
+        return []
+
+
